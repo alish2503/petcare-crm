93d45d82dd7af6046f24c545276f59cf81269a75
Alish <ali.tomash@mail.ru>
2025-08-21 21:10:34 +0500

    Added README

--- src/test/java/com/vet_care/demo/repository/DoctorRepositoryTest.java
+++ src/test/java/com/vet_care/demo/repository/DoctorRepositoryTest.java
@@ -6,6 +6,7 @@ import com.vet_care.demo.model.Doctor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
